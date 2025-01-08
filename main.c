fd7330e4f859ecd621ff7b11b0ff10dce4da0c03
CamiloGuz14 <camilo.guzman@udla.edu.ec>
2025-01-08 11:44:34 -0500

    main.c

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ int main (int argc, char *argv[]) {
   
     char texto[50];
     printf("Escribe algo: ");
-    
-    fgets(texto, sizeof(texto), stdin); 
+
+    fgets(texto, 50, stdin); 
     int len = strlen(texto);
 
     if (texto[len - 1] == '\n') {
@@ -14,28 +14,29 @@ int main (int argc, char *argv[]) {
         len--;
     }
 
-    int palabraLen = 0, palabras = 0;
-    int maxLen = 0, minLen = 50; 
+    int cont = 0, palabras = 0;
+    int max = 0, min = 50; 
 
     for (int i = 0; i <= len; i++) {
-        if (!isspace(texto[i]) && texto[i] != '\0') { 
-            palabraLen++;
-        } else if (palabraLen > 0) { 
+        if (texto [i] != ' ' && texto [i] != '\0') {
+            cont++;
+        } else if (cont > 0) { 
             palabras++;
-            if (palabraLen > maxLen) maxLen = palabraLen; 
-            if (palabraLen < minLen) minLen = palabraLen; 
-            palabraLen = 0; 
+            if (cont > max) max = cont; 
+            if (cont < min) min = cont; 
+            cont = 0; 
         }
     }
 
     printf("El texto tiene %d letras.\n", len);
     printf("Total de palabras: %d\n", palabras);
     if (palabras > 0) {
-        printf("Longitud de la palabra mas corta: %d\n", minLen);
-        printf("Longitud de la palabra mas larga: %d\n", maxLen);
+        printf("Longitud de la palabra mas corta: %d\n", min);
+        printf("Longitud de la palabra mas larga: %d\n", max);
     } else {
         printf("No se ingresaron palabras.\n");
     }
 
     return 0;
 }
+
